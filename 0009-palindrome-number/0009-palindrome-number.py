e48a27fe20f3394db3c51be324c2cf7fd5011c1a
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2022-12-29 15:25:22 -0500

    Time: 71 ms (77.63%), Space: 13.9 MB (59.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -11,8 +11,6 @@ class Solution:
         # rev_str = x_str[::-1]
         # return rev_str == x_str
         
-        return str(x) == str(x)[::-1]
-        
         # check len(string) - odd or even?
          # initialize 2 pointers - start and end
 #         start = 0
@@ -34,3 +32,28 @@ class Solution:
         # rev_str = ""
         # for each in range(x_len-1, -1, -1):
         #     rev_str += x_str[each]
+        
+         # return str(x) == str(x)[::-1]
+            
+        """
+        return if it is a negative integer
+        initialize rev_number = 0
+        while given_number > 0:
+            unit_place of given_number
+            multiply rev_num by 10
+            add unit_place to rev_number
+            divide given_number by 10
+        check rev_number == given_number ?
+        
+        """    
+        if x < 0:
+            return False
+        rev_num = 0
+        given_num = x
+        while x > 0:
+            units_place = x % 10
+            rev_num *= 10
+            rev_num += units_place
+            x = x // 10
+        return given_num == rev_num
+        
