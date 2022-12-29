50680779e09f1ecdca640c37abf8964752761515
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2022-12-29 15:16:57 -0500

    Time: 65 ms (84.81%), Space: 13.9 MB (59.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,16 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         # check if integer is negative - return false
-        if x < 0:
-            return False
-        elif x==0:
-            return True
+        # if x < 0:
+        #     return False
+        # elif x==0:
+        #     return True
         
         # convert x into string
-        x_str = str(x)
+        # x_str = str(x)
+        # rev_str = x_str[::-1]
+        # return rev_str == x_str
+        
+        return str(x) == str(x)[::-1]
         
         # check len(string) - odd or even?
-        x_len = len(x_str)
          # initialize 2 pointers - start and end
 #         start = 0
 #         end = x_len-1
@@ -31,7 +34,3 @@ class Solution:
         # rev_str = ""
         # for each in range(x_len-1, -1, -1):
         #     rev_str += x_str[each]
-        
-        rev_str = x_str[::-1]
-        
-        return rev_str == x_str
