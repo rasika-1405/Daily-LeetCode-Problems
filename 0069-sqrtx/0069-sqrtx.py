5f755a6c59a8833cf2fcbe3aa553da586e1d60a9
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-01-05 00:32:18 -0500

    Time: 79 ms (41.29%), Space: 14 MB (9.58%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,20 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x ** 0.5)
+        if x < 2:
+            return x
+        
+        #using binary search
+        min = 0
+        max = x
+        mid = (min+max)//2
+        
+        while min<max:
+            temp = mid * mid
+            if temp == x:
+                return mid
+            elif temp < x:
+                min = mid + 1
+            else:
+                max = mid
+            mid = (min+max) // 2
+        return mid - 1
