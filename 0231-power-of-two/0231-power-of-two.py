8b60580e82fc223740c5b3a58dac145c2c3f6e83
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-01-31 00:47:46 -0500

    Time: 36 ms (65.33%), Space: 13.9 MB (7.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,10 +1,23 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        # edge case
+        # # approach -1
+        # # edge case
+        # if n<=0:
+        #     return False
+        # x = log(n) / log(2)
+        # if x - int(x) <= 0.000000001:
+        #     return True
+        # return False
+        
+        # approach - 2
         if n<=0:
             return False
-        x = log(n) / log(2)
-        print (x)
-        if x - int(x) <= 0.000000001:
-            return True
-        return False
+        
+        out = str(bin(n))[2:]
+        count = 0
+        for i in range(len(out)):
+            if out[i] == '1':
+                count += 1
+            if count > 1:
+                return False
+        return True
