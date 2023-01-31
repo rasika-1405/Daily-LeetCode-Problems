2982c52cfa67f0d5c53dc82a67702b5e7de7a6d9
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-01-31 00:34:40 -0500

    Time: 30 ms (87.03%), Space: 13.9 MB (45.22%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,10 +1,10 @@
-from collections import Counter
+# from collections import Counter
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # out = str(bin(n))[2:]
-        # count = 0
-        # for i in range(len(out)):
-        #     if out[i] == '1':
-        #         count += 1
-        # return count
-        return Counter(bin(n))['1']
+        out = str(bin(n))[2:]
+        count = 0
+        for i in range(len(out)):
+            if out[i] == '1':
+                count += 1
+        return count
+        # return Counter(bin(n))['1']
