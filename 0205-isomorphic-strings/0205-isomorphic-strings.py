84f7dd87386ee950e5b0bbaedc08337b4fbfc48f
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-01-30 21:39:14 -0500

    Time: 31 ms (98.18%), Space: 14.3 MB (42.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -4,25 +4,40 @@ class Solution:
         # check base case - not required here
         if len(s) != len(t):
             return False
-
-        # require 2 maps
+        
+        # implementation using a map and a set
         s_map = {}
-        t_map = {}
+        t_set = set()
         
         for i in range(len(s)):
-            s_char = s[i]
-            t_char = t[i]
-            
-            if s_char in s_map:
-                if s_map[s_char] != t_char:
+            if s[i] in s_map:
+                if s_map[s[i]] != t[i]:
                     return False
             else:
-                s_map[s_char] = t_char
-            
-            if t_char in t_map:
-                if t_map[t_char] != s_char:
+                if t[i] in t_set:
                     return False
-            else:
-                t_map[t_char] = s_char
+                else:
+                    t_set.add(t[i])
+                    s_map[s[i]] = t[i]
+
+        # implementation using 2 maps
+#         s_map = {}
+#         t_map = {}
+        
+#         for i in range(len(s)):
+#             s_char = s[i]
+#             t_char = t[i]
+            
+#             if s_char in s_map:
+#                 if s_map[s_char] != t_char:
+#                     return False
+#             else:
+#                 s_map[s_char] = t_char
+            
+#             if t_char in t_map:
+#                 if t_map[t_char] != s_char:
+#                     return False
+#             else:
+#                 t_map[t_char] = s_char    
             
         return True
