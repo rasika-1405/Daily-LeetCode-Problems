40431730a628ce8867480f934f66037bc96cc351
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-02-07 22:20:13 -0500

    Time: 36 ms (64.04%), Space: 13.9 MB (88.22%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -1,15 +1,15 @@
 class Solution:
-    global dirs
-    global m
-    global n
+    # global dirs
+    # global m
+    # global n
     # global board
     
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        self.m = len(board)
-        self.n = len(board[0])
+        m = len(board)
+        n = len(board[0])
         
         # null case
         if board is None:
@@ -17,31 +17,31 @@ class Solution:
         
         # 1->0 mark as 3
         # 0->1 mark as 2
-        self.dirs = [[-1,0], [1,0], [0, -1], [0, 1], [-1,-1], [-1, 1], [1, 1], [1, -1]]
         
-        for i in range(self.m):
-            for j in range(self.n):
-                live_neighbors = self.countAlive(board, i, j)
+        for i in range(m):
+            for j in range(n):
+                live_neighbors = self.countAlive(board, i, j, m, n)
                 if board[i][j] == 1 and (live_neighbors<2 or live_neighbors>3):
                     board[i][j] = 3
                 if board[i][j] == 0 and live_neighbors == 3:
                     board[i][j] = 2
                     
-        for i in range(self.m):
-            for j in range(self.n):
+        for i in range(m):
+            for j in range(n):
                 if board[i][j] == 3:
                     board[i][j] = 0
                 if board[i][j] == 2:
                     board[i][j] = 1
         
     
-    def countAlive(self, board, i, j):
+    def countAlive(self, board, i, j, m, n):
+        dirs = [[-1,0], [1,0], [0, -1], [0, 1], [-1,-1], [-1, 1], [1, 1], [1, -1]]
         result = 0
-        for direction in self.dirs:
+        for direction in dirs:
             new_r = i + direction[0]
             new_c = j + direction[1]
             
-            if new_r >= 0 and new_c >= 0 and new_r < self.m and new_c < self.n and (board[new_r][new_c] == 1 or board[new_r][new_c] == 3):
+            if new_r >= 0 and new_c >= 0 and new_r < m and new_c < n and (board[new_r][new_c] == 1 or board[new_r][new_c] == 3):
                 result += 1
         
         return result
