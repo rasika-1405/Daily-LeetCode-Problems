4124099752c7bf1b51eea3a4206fcfd5fef84f36
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-02-11 19:39:57 -0500

    Time: 59 ms (89.77%), Space: 15.2 MB (18.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # if len(nums) == 1 and nums[0] == target:
-        #     return 0
-        # two_sum = []
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i]+nums[j] == target:
-        #             two_sum.append([i,j])
-        # return two_sum[0]
         result = []
         if nums is None or len(nums) == 1:
             return result
         
         # brute force
         
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    result.append([i, j])
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     result.append([i, j])
+        
+        # using hashing
+        
+        sum_map = {}
+        for i in range(len(nums)):
+            k = target - nums[i]
+            if k in sum_map:
+                result.append([sum_map[k], i])
+            else:
+                sum_map[nums[i]] = i
+        
         return result[0]
+    
+        
+    
+        
