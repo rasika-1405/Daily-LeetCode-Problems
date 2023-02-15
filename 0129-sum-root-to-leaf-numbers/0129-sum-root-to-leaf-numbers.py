7871f78910dc779b1f3ea9664ef9b1568e409c8a
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-02-14 21:55:33 -0500

    Time: 37 ms (41.83%), Space: 13.8 MB (96.55%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -5,28 +5,41 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    result = 0
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         # iterative method
-        st1 = []  #stack for current node
-        st2 = []   # number
+#         st1 = []  #stack for current node
+#         st2 = []   # number
         
-        curr_num = 0
-        result = 0
+#         curr_num = 0
+#         result = 0
         
-        while root != None or len(st1) != 0:
-            while root != None:
-                curr_num = curr_num * 10 + root.val
-                st1.append(root)
-                st2.append(curr_num)
-                root = root.left
+#         while root != None or len(st1) != 0:
+#             while root != None:
+#                 curr_num = curr_num * 10 + root.val
+#                 st1.append(root)
+#                 st2.append(curr_num)
+#                 root = root.left
                 
-            root = st1.pop()
-            curr_num = st2.pop()
+#             root = st1.pop()
+#             curr_num = st2.pop()
             
-            if root.left == None and root.right == None:
-                result += curr_num
-            root = root.right
+#             if root.left == None and root.right == None:
+#                 result += curr_num
+#             root = root.right
+
+        # recursive method
+        self.helper(root, 0)
+        return self.result
+
+    def helper(self, root, curr_num):
+        # base case
+        if root == None:
+            return
+        # check leaf condition
+        if root.left == None and root.right == None:
+            self.result += curr_num * 10 + root.val
         
-        return result
-            
+        self.helper(root.left, curr_num * 10 + root.val)
+        self.helper(root.right, curr_num * 10 + root.val)
         
