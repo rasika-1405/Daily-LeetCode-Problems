4053b9c51acfab49ce2a6c133ada5ef126a56f46
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-02-15 22:23:00 -0500

    Time: 43 ms (77.94%), Space: 15 MB (48.79%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -9,15 +9,13 @@ class Solution:
         def helper(root, curr_sum, target):
             # base case
             if root == None:
-                return
+                return False
             
             curr_sum += root.val
             if root.left == None and root.right == None:
                 if curr_sum == target:
-                    self.flag = True
-            helper(root.left, curr_sum, target)
-            helper(root.right, curr_sum, target)
-                        
-        self.flag = False
-        helper(root, 0, targetSum)
-        return self.flag
+                    return True
+            
+            return helper(root.left, curr_sum, target) or helper(root.right, curr_sum, target)            
+            
+        return helper(root, 0, targetSum)
