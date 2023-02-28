cd8c0fe52374aa934c670a225cc86b36fc21eaf5
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-02-27 22:17:36 -0500

    Time: 97 ms (73.08%), Space: 13.7 MB (99.10%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -8,20 +8,21 @@ class Solution:
         set_cols = [set() for _ in range(9)]
         grid_map = {}
         
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if board[r][c] in set_rows[r] or board[r][c] in set_cols[c]:
-                    return False
-                set_rows[r].add(board[r][c])
-                set_cols[c].add(board[r][c])
-                
-                # check if grid coordinates exists
-                if (r//3, c//3) not in grid_map:
-                    grid_map[(r//3, c//3)] = set()
-                if board[r][c] in grid_map[(r//3, c//3)]:
-                    return False
-                grid_map[(r//3, c//3)].add(board[r][c])
+        for i in range(81):
+            r = i // 9
+            c = i % 9
+            if board[r][c] == ".":
+                continue
+            if board[r][c] in set_rows[r] or board[r][c] in set_cols[c]:
+                return False
+            set_rows[r].add(board[r][c])
+            set_cols[c].add(board[r][c])
+
+            # check if grid coordinates exists
+            if (r//3, c//3) not in grid_map:
+                grid_map[(r//3, c//3)] = set()
+            if board[r][c] in grid_map[(r//3, c//3)]:
+                return False
+            grid_map[(r//3, c//3)].add(board[r][c])
                 
         return True
