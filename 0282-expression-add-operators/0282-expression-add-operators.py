a3bbe8b19769c0634c1db162008bd62aed973c8a
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-02-28 23:50:17 -0500

    Time: 1857 ms (15.46%), Space: 14.6 MB (78.55%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.py
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -17,14 +17,27 @@ class Solution:
             # preceding 0
             if num[pivot] == "0" and i!= pivot:
                 continue
+            length = len(path)
             curr = int(num[pivot:i+1])
             # need to handle first level separation (no operators)
             if pivot == 0:
-                self.helper(num, i+1, target, path+str(curr), curr, curr)
+                #action
+                path = "".join(path + str(curr))
+                self.helper(num, i+1, target, path, curr, curr)
+                path = path[:length]
             else:
                 # case +
-                self.helper(num, i+1, target, path+"+"+str(curr), calculated_val+curr, +curr)
+                path = path + "+"
+                path = "".join(path + str(curr))
+                self.helper(num, i+1, target, path, calculated_val+curr, +curr)
+                path = path[:length]
                 # case -
-                self.helper(num, i+1, target, path+"-"+str(curr), calculated_val-curr, -curr)
+                path = path + "-"
+                path = "".join(path + str(curr))
+                self.helper(num, i+1, target, path, calculated_val-curr, -curr)
+                path = path[:length]
                 # case *
-                self.helper(num, i+1, target, path+"*"+str(curr), calculated_val-tail_val+tail_val*curr, tail_val*curr)
+                path = path + "*"
+                path = "".join(path + str(curr))
+                self.helper(num, i+1, target, path, calculated_val-tail_val+tail_val*curr, tail_val*curr)
+                path = path[:length]
