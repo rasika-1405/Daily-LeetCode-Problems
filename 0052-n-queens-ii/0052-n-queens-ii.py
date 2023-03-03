d1e575d1329745f81246a1ab8b5dc453be867a1e
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-02 22:10:35 -0500

    Time: 83 ms (35.96%), Space: 14.1 MB (14.79%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,48 +1,48 @@
 class Solution:
-    m = 0
     count = 0
     def totalNQueens(self, n: int) -> int:
         self.count = 0
-        self.m = n
+        # self.m = n
         board = [[False for _ in range(n)] for _ in range(n)]
-        self.backtrack(board, 0)
+    
+    
+        def backtrack(board, r):
+            # base case
+            if r == n:
+                self.count += 1
+
+
+            #logic
+            # check safe column
+            for c in range(n):
+                if (isSafe(board, r, c)):
+                    board[r][c] = True # action
+                    backtrack(board, r+1) # recurse
+                    board[r][c] = False # backtrack
+
+
+        def isSafe(board, r, c):
+            # same column
+            for i in range(r):
+                if board[i][c]:
+                    return False
+            #upper left
+            i,j = r,c
+            while i>=0 and j>=0:
+                if board[i][j]:
+                    return False
+                i -= 1
+                j -= 1
+
+            #upper right
+            i,j = r,c
+            while i>=0 and j<n:
+                if board[i][j]:
+                    return False
+                i -= 1
+                j += 1
+
+            return True
+        
+        backtrack(board, 0)
         return self.count
-    
-    
-    def backtrack(self, board, r):
-        # base case
-        if r == self.m:
-            self.count += 1
-                
-        
-        #logic
-        # check safe column
-        for c in range(self.m):
-            if (self.isSafe(board, r, c)):
-                board[r][c] = True # action
-                self.backtrack(board, r+1) # recurse
-                board[r][c] = False # backtrack
-        
-
-    def isSafe(self, board, r, c):
-        # same column
-        for i in range(r):
-            if board[i][c]:
-                return False
-        #upper left
-        i,j = r,c
-        while i>=0 and j>=0:
-            if board[i][j]:
-                return False
-            i -= 1
-            j -= 1
-
-        #upper right
-        i,j = r,c
-        while i>=0 and j<self.m:
-            if board[i][j]:
-                return False
-            i -= 1
-            j += 1
-
-        return True
