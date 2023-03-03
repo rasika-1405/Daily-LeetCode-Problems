be050d65d507e8e979641de1efe1a73a30c7b10b
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-02 21:10:01 -0500

    Time: 105 ms (35.11%), Space: 14.5 MB (36.80%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,57 @@
+class Solution:
+    m = 0
+    result = []
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        self.result = []
+        self.m = n
+        board = [[False for _ in range(n)] for _ in range(n)]
+        self.backtrack(board, 0)
+        return self.result
+    
+    
+    def backtrack(self, board, r):
+        # base case
+        if r == self.m:
+            res = []
+            for i in range(self.m):
+                sb = ""
+                for j in range(self.m):
+                    if board[i][j]:
+                        sb += "Q"
+                    else:
+                        sb += "."
+                res.append(sb)
+            self.result.append(res)
+                
+        
+        #logic
+        # check safe column
+        for c in range(self.m):
+            if (self.isSafe(board, r, c)):
+                board[r][c] = True # action
+                self.backtrack(board, r+1) # recurse
+                board[r][c] = False # backtrack
+        
+
+    def isSafe(self, board, r, c):
+        # same column
+        for i in range(r):
+            if board[i][c]:
+                return False
+        #upper left
+        i,j = r,c
+        while i>=0 and j>=0:
+            if board[i][j]:
+                return False
+            i -= 1
+            j -= 1
+
+        #upper right
+        i,j = r,c
+        while i>=0 and j<self.m:
+            if board[i][j]:
+                return False
+            i -= 1
+            j += 1
+
+        return True
