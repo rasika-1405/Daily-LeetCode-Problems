04a409daa62a75b62fed8b21bfee461cc5022360
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-06 23:42:06 -0500

    Time: 79 ms (20.17%), Space: 17.8 MB (31.67%) - LeetHub

--- 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.py
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.py
@@ -21,29 +21,57 @@
 #        """
 
 class NestedIterator:
-    li = []
-    count = 0
+    
     def __init__(self, nestedList: [NestedInteger]):
-        # self.ni = NestedInteger()
-        self.li = []
-        self.dfs(nestedList)
-    
-    def dfs(self, nested_list):        
-        # logic
-        for nl in nested_list:
-            if nl.isInteger():
-                self.li.append(nl.getInteger())
-            else:
-                self.dfs(nl.getList())
-    
+        self.stk = [[nestedList, 0]]
+        
     def next(self) -> int:
-        result = self.li[self.count]
-        self.count += 1
-        return result
+        self.hasNext()
+        last_element, iterator = self.stk[-1]
+        self.stk[-1][1] += 1
+        return last_element[iterator].getInteger()
+        
+        
+    def hasNext(self) -> bool:
+        s = self.stk
+        
+        while s:
+            nested_list, iterator = s[-1]
+            if iterator == len(nested_list):
+                s.pop()
+            else:
+                curr = nested_list[iterator]
+                if curr.isInteger():
+                    return True
+                else:
+                    s[-1][1] += 1
+                    s.append([curr.getList(), 0])
+        return False
+        
+    # Brute force
+#     li = []
+#     count = 0
+#     def __init__(self, nestedList: [NestedInteger]):
+#         # self.ni = NestedInteger()
+#         self.li = []
+#         self.dfs(nestedList)
+    
+#     def dfs(self, nested_list):        
+#         # logic
+#         for nl in nested_list:
+#             if nl.isInteger():
+#                 self.li.append(nl.getInteger())
+#             else:
+#                 self.dfs(nl.getList())
+    
+#     def next(self) -> int:
+#         result = self.li[self.count]
+#         self.count += 1
+#         return result
         
     
-    def hasNext(self) -> bool:
-         return self.count != len(self.li)
+#     def hasNext(self) -> bool:
+#          return self.count != len(self.li)
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
