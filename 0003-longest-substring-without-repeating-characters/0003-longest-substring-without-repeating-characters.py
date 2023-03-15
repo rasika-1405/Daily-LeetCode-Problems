1fc60250a625ee71ace7fdce677be401c08a83a3
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-15 16:32:15 -0400

    Time: 68 ms (58.46%), Space: 14 MB (41.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,16 +3,37 @@ class Solution:
         # null case
         if len(s) == 0:
             return 0
+        if len(s) == 1:
+            return 1
         
-        char_map = {}
-        slow = 0
         max_len = 0
         
-        for fast in range(len(s)):
+        # Using Hashmap
+#         char_map = {}
+#         slow = 0
+        
+#         for fast in range(len(s)):
+#             char = s[fast]
+#             if char in char_map:
+#                 slow = max(slow, char_map[char]+1)
+#             max_len = max(max_len, fast-slow+1)
+#             char_map[char] = fast
+
+        # Using Hashsets
+        char_set = set()
+        char_set.add(s[0])
+        slow, fast = 0, 1
+        
+        while fast < len(s):
             char = s[fast]
-            if char in char_map:
-                slow = max(slow, char_map[char]+1)
+            if char in char_set:
+                while s[slow] != char:
+                    char_set.remove(s[slow])
+                    slow += 1
+                slow += 1
             max_len = max(max_len, fast-slow+1)
-            char_map[char] = fast
+            char_set.add(char)
+            fast += 1
+        
         return max_len
         
