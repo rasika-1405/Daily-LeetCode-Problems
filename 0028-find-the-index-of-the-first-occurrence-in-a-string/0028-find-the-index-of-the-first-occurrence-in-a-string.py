853c4fc0c181edaa0f0796e50bb703ead19ca6e8
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-14 22:53:42 -0400

    Time: 33 ms (52.21%), Space: 13.8 MB (95.04%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,7 +3,7 @@ class Solution:
         
         def cal_lps(needle):
             lps = [0 for _ in range(len(needle))]
-            i, j = 0,1
+            i, j = 1,0
             while i<len(needle):
                 if needle[j] == needle[i]:
                     j+=1
@@ -20,7 +20,7 @@ class Solution:
         n = len(needle)
         
         # 2 pointers
-        # i,j = 0,0
+        i,j = 0,0
         
         # Brute force
         # while i <= m-n:
@@ -40,45 +40,45 @@ class Solution:
             return -1
         
         # Using KMP
-#         lps = cal_lps(needle)
+        lps = cal_lps(needle)
         
-#         while i<m:
-#             if haystack[i] == needle[j]:
-#                 i+=1
-#                 j+=1
-#                 if j==n:
-#                     return i-n
-#             elif haystack[i] != needle[j] and j>0:
-#                 j = lps[j-1]
-#             elif haystack[i] != needle[j] and j==0:
-#                 i+=1
-#         return -1
+        while i<m:
+            if haystack[i] == needle[j]:
+                i+=1
+                j+=1
+                if j==n:
+                    return i-n
+            elif haystack[i] != needle[j] and j>0:
+                j = lps[j-1]
+            elif haystack[i] != needle[j] and j==0:
+                i+=1
+        return -1
 
         # Using Robin Karp
-        k = 26
-        k1 = pow(k, n)
-        needle_hash = 0
+#         k = 26
+#         k1 = pow(k, n)
+#         needle_hash = 0
         
-        for i in range(len(needle)):
-            char = needle[i]
-            needle_hash = needle_hash * k + (ord(char) - ord('a'))
+#         for i in range(len(needle)):
+#             char = needle[i]
+#             needle_hash = needle_hash * k + (ord(char) - ord('a'))
             
-        # making a window of length needle on haystack
-        haystack_hash = 0
-        for i in range(n):
-            char = haystack[i]
-            haystack_hash = haystack_hash * k + (ord(char) - ord('a'))
-        if haystack_hash == needle_hash:
-            return 0
+#         # making a window of length needle on haystack
+#         haystack_hash = 0
+#         for i in range(n):
+#             char = haystack[i]
+#             haystack_hash = haystack_hash * k + (ord(char) - ord('a'))
+#         if haystack_hash == needle_hash:
+#             return 0
         
-        # slide the window
-        for i in range(1, m-n+1):
-            in_char = haystack[i+n-1]
-            haystack_hash = haystack_hash * k + (ord(in_char) - ord('a'))
+#         # slide the window
+#         for i in range(1, m-n+1):
+#             in_char = haystack[i+n-1]
+#             haystack_hash = haystack_hash * k + (ord(in_char) - ord('a'))
             
-            out_char = haystack[i-1]
-            haystack_hash = haystack_hash - k1 * (ord(out_char) - ord('a'))
+#             out_char = haystack[i-1]
+#             haystack_hash = haystack_hash - k1 * (ord(out_char) - ord('a'))
             
-            if haystack_hash == needle_hash:
-                return i
-        return -1
+#             if haystack_hash == needle_hash:
+#                 return i
+#         return -1
