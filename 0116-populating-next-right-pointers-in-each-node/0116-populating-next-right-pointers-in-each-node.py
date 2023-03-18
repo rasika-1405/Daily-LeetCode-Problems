df32fab4eb9d1e9ee4fba475c4125b19a9c03f4e
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-17 21:46:52 -0400

    Time: 180 ms (5.02%), Space: 15.8 MB (42.95%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -13,30 +13,45 @@ class Solution:
         if root is None:
             return None
         
-#         queue = deque([root])
-        
-#         while queue:
-#             size = len(queue)
-#             prev = None
-#             for i in range(size):
-#                 curr = queue.popleft()
-#                 if i!= 0:
-#                     prev.next = curr
-#                 if curr.left:
-#                     queue.append(curr.left)
-#                     queue.append(curr.right) #complete binary tree
-#                 prev = curr
+        # Using BFS - queue
+        # queue = deque([root])
+        # while queue:
+        #     size = len(queue)
+        #     prev = None
+        #     for i in range(size):
+        #         curr = queue.popleft()
+        #         if i!= 0:
+        #             prev.next = curr
+        #         if curr.left:
+        #             queue.append(curr.left)
+        #             queue.append(curr.right) #complete binary tree
+        #         prev = curr
                 
         # optimized BFS
-        level = root
-        while level.left:
-            curr = level
-            while curr:
-                curr.left.next = curr.right
-                if curr.next:
-                    curr.right.next = curr.next.left
-                curr = curr.next
-            level = level.left
+        # level = root
+        # while level.left:
+        #     curr = level
+        #     while curr:
+        #         curr.left.next = curr.right
+        #         if curr.next:
+        #             curr.right.next = curr.next.left
+        #         curr = curr.next
+        #     level = level.left
+        
+        # using DFS
+        def dfs(left, right):
+            # base case
+            if left is None:
+                return
+            
+            # logic
+            left.next = right
+            dfs(left.left, left.right)
+            dfs(left.right, right.left)
+            dfs(right.left, right.right)
+        
+        dfs(root.left, root.right)
+        
         
         return root
                 
