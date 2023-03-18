7c8be88ee16034acee4a0dcd0980f1ff01b66f6f
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-17 21:27:24 -0400

    Time: 49 ms (99.56%), Space: 15.5 MB (71.89%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -13,19 +13,30 @@ class Solution:
         if root is None:
             return None
         
-        queue = deque([root])
+#         queue = deque([root])
         
-        while queue:
-            size = len(queue)
-            prev = None
-            for i in range(size):
-                curr = queue.popleft()
-                if i!= 0:
-                    prev.next = curr
-                if curr.left:
-                    queue.append(curr.left)
-                    queue.append(curr.right) #complete binary tree
-                prev = curr
+#         while queue:
+#             size = len(queue)
+#             prev = None
+#             for i in range(size):
+#                 curr = queue.popleft()
+#                 if i!= 0:
+#                     prev.next = curr
+#                 if curr.left:
+#                     queue.append(curr.left)
+#                     queue.append(curr.right) #complete binary tree
+#                 prev = curr
                 
+        # optimized BFS
+        level = root
+        while level.left:
+            curr = level
+            while curr:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            level = level.left
+        
         return root
                 
