1b3c148e2ce6e9328bc51b5d91110b5e5f597f4c
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-20 21:26:52 -0400

    Time: 679 ms (60.65%), Space: 16.9 MB (61.23%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        # null case
+        if matrix is None:
+            return 0
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        max_sq = 0
+        
+        # Brute force
+        # flag = False # when we hit a square
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j] == '1':
+        #             flag = True
+        #             k = 1 #side
+        #             while i+k<m and j+k<n and flag:
+        #                 for r in range(i+k,i-1,-1):
+        #                     if matrix[r][j+k] == '0':
+        #                         flag = False
+        #                         break
+        #                 for c in range(j+k,j-1,-1):
+        #                     if matrix[i+k][c] == '0':
+        #                         flag = False
+        #                         break
+        #                 if flag:
+        #                     k+=1
+        #             max_sq = max(max_sq, k)
+        # return max_sq*max_sq
+        
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if matrix[i-1][j-1] == '1':
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])
+                    max_sq = max(max_sq, dp[i][j])
+        return max_sq*max_sq
