8a35572891bcb346f564f2a0a99f47bef0c3ef7c
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-21 21:17:28 -0400

    Time: 34 ms (55.81%), Space: 14.4 MB (5.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -17,28 +17,26 @@ class Solution:
         # return dfs(0,0,m,n)
         
         # Top-down DP
-        # memo = [[0]*n]*m
-        # def dfs_memo(i,j,m,n):
-        #     # base case
-        #     if i == m-1 and j == n-1:
-        #         return 1
-        #     if i<0 or j<0 or i == m or j == n:
-        #         return 0
-        #     #logic
-        #     if memo[i][j] == 0:
-        #         memo[i][j] = dfs_memo(i,j+1,m,n) + dfs_memo(i+1,j,m,n)
-        #     return memo[i][j]
-        # return dfs_memo(0,0,m,n)
+        memo = [[0 for _ in range(n)] for _ in range(m)]
+        def dfs_memo(i,j,m,n):
+            # base case
+            if i == m-1 and j == n-1:
+                return 1
+            if i<0 or j<0 or i == m or j == n:
+                return 0
+            #logic
+            if memo[i][j] == 0:
+                memo[i][j] = dfs_memo(i,j+1,m,n) + dfs_memo(i+1,j,m,n)
+            return memo[i][j]
+        return dfs_memo(0,0,m,n)
         
         # Bottom-up DP
-        dp = [[0]*(n+1)]*(m+1)
-        dp[m-1][n-1] = 1
-        
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if i == m-1 and j == n-1:
-                    continue
-                dp[i][j] = dp[i+1][j] + dp[i][j+1]
-                
-        return dp[0][0]
+        # dp = [[0]*(n+1)]*(m+1)
+        # dp[m-1][n-1] = 1
+        # for i in range(m-1, -1, -1):
+        #     for j in range(n-1, -1, -1):
+        #         if i == m-1 and j == n-1:
+        #             continue
+        #         dp[i][j] = dp[i+1][j] + dp[i][j+1]
+        # return dp[0][0]
     
