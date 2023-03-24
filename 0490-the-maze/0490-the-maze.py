a9c9189251bcfbc80fb64869eea1b4b3bce7a83c
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-23 21:32:46 -0400

    Time: 261 ms (95.00%), Space: 15.6 MB (43.87%) - LeetHub

--- 0490-the-maze/0490-the-maze.py
+++ 0490-the-maze/0490-the-maze.py
@@ -8,23 +8,46 @@ class Solution:
         if maze is None:
             return False
         
-        queue = deque([start])
-        maze[start[0]][start[1]] = 2 # mark as visited
+#         queue = deque([start])
+#         maze[start[0]][start[1]] = 2 # mark as visited
         
-        while queue:
-            curr = queue.popleft()
+#         while queue:
+#             curr = queue.popleft()
+#             for dirn in dirs:
+#                 i = curr[0]
+#                 j = curr[1]
+                
+#                 while i>=0 and j>=0 and i<m and j<n and maze[i][j] != 1:
+#                     i+=dirn[0]
+#                     j+=dirn[1]
+#                 i-= dirn[0]
+#                 j-= dirn[1]
+#                 if maze[i][j] != 2:
+#                     queue.append([i,j])
+#                     if i == destination[0] and j == destination[1]:
+#                         return True
+#                     maze[i][j] = 2
+            
+        def dfs(maze, curr, desn):
+            # base case
+            if curr[0] == desn[0] and curr[1] == desn[1]:
+                return True
+            if maze[curr[0]][curr[1]] == 2:
+                return False
+            
+            # logic
+            maze[curr[0]][curr[1]] = 2 # mark as visited
             for dirn in dirs:
                 i = curr[0]
                 j = curr[1]
                 
                 while i>=0 and j>=0 and i<m and j<n and maze[i][j] != 1:
                     i+=dirn[0]
-                    j+= dirn[1]
+                    j+=dirn[1]
                 i-= dirn[0]
                 j-= dirn[1]
-                if maze[i][j] != 2:
-                    queue.append([i,j])
-                    if i == destination[0] and j == destination[1]:
-                        return True
-                    maze[i][j] = 2
-        return False
+                if dfs(maze, [i,j], desn):
+                    return True     
+            return False
+        
+        return dfs(maze, start, destination)
