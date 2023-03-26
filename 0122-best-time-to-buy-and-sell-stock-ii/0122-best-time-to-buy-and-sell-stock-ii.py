acff0ed03c5039fdf285901e7b7e3d9511199e7b
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-26 13:31:39 -0400

    Time: 55 ms (94.85%), Space: 15.2 MB (18.45%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # null case
+        if prices is None:
+            return 0
+        
+        n = len(prices)
+        max_profit = 0
+        
+        for i in range(n-1):
+            # valley condition
+            if prices[i] > prices[i+1]:
+                continue
+            profit = prices[i+1] - prices[i]
+            max_profit += profit
+        return max_profit
+        
