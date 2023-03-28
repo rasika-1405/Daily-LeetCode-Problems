9e0cd6b28ef8f6ba27269f64024e6597dc3f0f44
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-03-27 22:08:24 -0400

    Time: 132 ms (96.81%), Space: 20.7 MB (21.05%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -6,8 +6,24 @@ class Solution:
         
         n = len(citations)
         
-        for i in range(n):
-            diff = n - i
-            if diff<=citations[i]: # h-index
+        # linear search
+        # for i in range(n):
+        #     diff = n - i
+        #     if diff<=citations[i]: # h-index
+        #         return diff
+        # return 0
+        
+        # binary search
+        low = 0
+        high = n-1
+        
+        while low<= high:
+            mid = low + (high-low) // 2
+            diff = n-mid
+            if citations[mid] == diff:
                 return diff
-        return 0
+            elif citations[mid] < diff:
+                low=mid+1
+            else:
+                high=mid-1
+        return n-low
