38001ffa765644efa2f2bbef2b770fd27aea7fd7
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-04-02 12:47:26 -0400

    Time: 65 ms (55.18%), Space: 15.1 MB (55.28%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -7,10 +7,26 @@ class Solution:
         n = len(prices)
         max_profit = 0
         
-        for i in range(n-1):
+        # for i in range(n-1):
+        #     # valley condition
+        #     if prices[i] > prices[i+1]:
+        #         continue
+        #     profit = prices[i+1] - prices[i]
+        #     max_profit += profit
+        # return max_profit
+        
+        peak = prices[0]
+        valley = prices[0]
+        i=0
+        
+        while i<n-1:
             # valley condition
-            if prices[i] > prices[i+1]:
-                continue
-            profit = prices[i+1] - prices[i]
-            max_profit += profit
+            if i<n-1 and prices[i] >= prices[i+1]:
+                i+=1
+            valley = prices[i]
+            if i<n-1 and prices[i] <= prices[i+1]:
+                i+=1
+            peak = prices[i]
+            max_profit += peak - valley
         return max_profit
+            
