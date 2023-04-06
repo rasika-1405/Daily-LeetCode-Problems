42e5b8ed334e46b78a39c468c448336ed7819dc7
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-04-05 22:12:12 -0400

    Time: 124 ms (89.99%), Space: 14.9 MB (87.07%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,22 +4,51 @@ class Solution:
             return 0
         
         # Using BFS
-        queue = deque()
-        visited = set()
-        queue.append(0)
-        visited.add(0)
+#         queue = deque()
+#         visited = set()
+#         queue.append(0)
+#         visited.add(0)
+#         jumps = 1
+        
+#         while queue:
+#             size = len(queue)
+#             for k in range(size):
+#                 i = queue.popleft()
+#                 for j in range(nums[i], -1, -1):
+#                     new_idx = i+j
+#                     if new_idx>=len(nums)-1:
+#                         return jumps
+#                     if new_idx not in visited:
+#                         queue.append(new_idx)
+#                         visited.add(new_idx)
+#             jumps+=1
+#         return 7209
+
+        # Using DFS
+#         min_jump = inf
+        
+#         def dfs(nums, i, jumps):
+#             # base case
+#             if i>= len(nums)-1:
+#                 min_jump = min(min_jump, jumps)
+            
+#             # logic
+#             for j in range(nums[i], -1, -1):
+#                 new_idx = i+j
+#                 dfs(nums, new_idx, jumps+1)
+            
+#         dfs(nums, 0, 0)
+#         return min_jump
+
+        # Using greedy approach
+        curr_interval = nums[0]
+        next_interval = nums[0]
         jumps = 1
         
-        while queue:
-            size = len(queue)
-            for k in range(size):
-                i = queue.popleft()
-                for j in range(nums[i], -1, -1):
-                    new_idx = i+j
-                    if new_idx>=len(nums)-1:
-                        return jumps
-                    if new_idx not in visited:
-                        queue.append(new_idx)
-                        visited.add(new_idx)
-            jumps+=1
-        return 7209
+        for i in range(1, len(nums)):
+            next_interval = max(next_interval, nums[i]+i)
+            if i == curr_interval and i!=len(nums)-1:
+                jumps+=1
+                curr_interval = next_interval
+        
+        return jumps
