c09ca7a9b476a9762bc338061ce38f1c76ffe854
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-04-06 21:09:16 -0400

    Time: 137 ms (57.62%), Space: 17.1 MB (9.49%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,15 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Brute force
-        hash_map = {}
+#         hash_map = {}
         
-        for num in nums:
-            if num in hash_map:
-                hash_map[num] += 1
-            else:
-                hash_map[num] = 1
+#         for num in nums:
+#             if num in hash_map:
+#                 hash_map[num] += 1
+#             else:
+#                 hash_map[num] = 1
         
-        for num in hash_map:
-            if hash_map[num] == 1:
-                return num
+#         for num in hash_map:
+#             if hash_map[num] == 1:
+#                 return num
             
+        # Using set
+        return 2* sum(set(nums)) - sum(nums)
