74e2b32fb1197e75b12aeeb5dc65aba44cc9ab42
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-04-10 21:52:40 -0400

    Time: 44 ms (27.91%), Space: 14.3 MB (97.11%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -13,18 +13,35 @@ class Solution:
             return None
         
         node_map = {}
-        queue = deque()
-        queue.append(node)
-        new_node = Node(node.val)
-        node_map[node] = new_node
         
-        while queue:
-            curr = queue.popleft()
-            neighbors = curr.neighbors
-            for neighbor in neighbors:
+        # Using BFS
+#         queue = deque()
+#         queue.append(node)
+#         new_node = Node(node.val)
+#         node_map[node] = new_node
+        
+#         while queue:
+#             curr = queue.popleft()
+#             neighbors = curr.neighbors
+#             for neighbor in neighbors:
+#                 if neighbor not in node_map:
+#                     deep_copy = Node(neighbor.val)
+#                     node_map[neighbor] = deep_copy
+#                     queue.append(neighbor)
+#                 node_map[curr].neighbors.append(node_map[neighbor])
+#         return new_node
+    
+        # Using DFS
+        def dfs(node):
+            # base case
+            
+            # logic
+            deep_copy = Node(node.val)
+            node_map[node] = deep_copy
+            for neighbor in node.neighbors:
                 if neighbor not in node_map:
-                    deep_copy = Node(neighbor.val)
-                    node_map[neighbor] = deep_copy
-                    queue.append(neighbor)
-                node_map[curr].neighbors.append(node_map[neighbor])
-        return new_node
+                    dfs(neighbor)
+                node_map[node].neighbors.append(node_map[neighbor])
+                
+        dfs(node)
+        return node_map[node]
