5e55f09b094aec824364491615a1f7e024cf9526
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-04-11 22:53:24 -0400

    Time: 178 ms (83.09%), Space: 15.5 MB (73.69%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,5 +1,37 @@
 class Solution:
     dirs = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1,1], [-1,-1]]
+    # Using BFS
+#     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+#         # null case
+#         if board is None:
+#             return board
+        
+#         m = len(board)
+#         n = len(board[0])
+        
+#         if board[click[0]][click[1]] == "M":
+#             board[click[0]][click[1]] = "X"
+#             return board
+        
+#         queue = deque()
+#         queue.append(click)
+#         board[click[0]][click[1]] = "B"
+        
+#         while queue:
+#             curr = queue.popleft()
+#             count = self.count_mines(board, curr[0], curr[1], m, n)
+#             if count == 0:
+#                 for dirn in self.dirs:
+#                     nr = dirn[0] + curr[0]
+#                     nc = dirn[1] + curr[1]
+#                     if nr>=0 and nr<m and nc>=0 and nc<n and board[nr][nc] == "E":
+#                         queue.append([nr, nc])
+#                         board[nr][nc] = "B"
+#             else:
+#                 board[curr[0]][curr[1]] = str(count)
+#         return board
+    
+    # Using DFS
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         # null case
         if board is None:
@@ -10,25 +42,27 @@ class Solution:
         
         if board[click[0]][click[1]] == "M":
             board[click[0]][click[1]] = "X"
-            return board
+            return board 
         
-        queue = deque()
-        queue.append(click)
-        board[click[0]][click[1]] = "B"
-        
-        while queue:
-            curr = queue.popleft()
-            count = self.count_mines(board, curr[0], curr[1], m, n)
+        def dfs(board, i, j, m, n):
+            # base case
+            if i<0 or i==m or j<0 or j==n or board[i][j] != "E":
+                return
+
+            # logic
+            board[i][j] = "B"
+            count = self.count_mines(board, i, j, m, n)
             if count == 0:
                 for dirn in self.dirs:
-                    nr = dirn[0] + curr[0]
-                    nc = dirn[1] + curr[1]
-                    if nr>=0 and nr<m and nc>=0 and nc<n and board[nr][nc] == "E":
-                        queue.append([nr, nc])
-                        board[nr][nc] = "B"
+                    nr = dirn[0] + i
+                    nc = dirn[1] + j
+                    dfs(board, nr, nc, m, n)
             else:
-                board[curr[0]][curr[1]] = str(count)
+                board[i][j] = str(count)
+        
+        dfs(board, click[0], click[1], m, n)
         return board
+    
     
     def count_mines(self, board, i, j, m, n):
         count = 0
