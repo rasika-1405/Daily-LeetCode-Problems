fed17b7bef4469645339850c47276592ac4fa6f4
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-04-24 22:16:06 -0400

    Time: 32 ms (95.75%), Space: 13.9 MB (68.71%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -3,25 +3,49 @@ class Solution:
         sl = len(source)
         tl = len(target)
         count = 1
-        
         sp, tp = 0, 0
-        char_set = set()
-        for char in source:
-            char_set.add(char)
+        
+        # brute force
+#         char_set = set()
+#         for char in source:
+#             char_set.add(char)
             
+#         while tp<tl:
+#             s_char = source[sp]
+#             t_char = target[tp]
+#             if t_char not in char_set:
+#                 return -1
+#             if s_char == t_char:
+#                 sp+=1
+#                 tp+=1
+#                 if tp == tl:
+#                     return count
+#             else:
+#                 sp+=1
+#             if sp == sl:
+#                 count += 1
+#                 sp=0
+        
+        # Optimal solution
+        char_map = defaultdict(list)
+        for i in range(sl):
+            char = source[i]
+            char_map[char].append(i)
+        
         while tp<tl:
-            s_char = source[sp]
             t_char = target[tp]
-            if t_char not in char_set:
+            if t_char not in char_map:
                 return -1
-            if s_char == t_char:
-                sp+=1
-                tp+=1
-                if tp == tl:
-                    return count
-            else:
-                sp+=1
-            if sp == sl:
+            t_list = char_map[t_char]
+            k = bisect_left(t_list, sp)
+            if k < 0:
+                k = -k-1
+            if k == len(t_list):
                 count += 1
-                sp=0
+                sp = t_list[0]
+            else:
+                sp = t_list[k]
+            sp+=1
+            tp+=1
+        
         return count
