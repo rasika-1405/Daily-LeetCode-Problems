648b14582b4f3342a4e18e961d2f2e033919cfd7
Rasika Sasturkar <72156659+rasika-1405@users.noreply.github.com>
2023-05-01 21:02:42 -0400

    Time: 255 ms (25.74%), Space: 25.1 MB (5.22%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -8,6 +8,7 @@ class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         result = 0
         
+        # void based recursion
         def helper(root, low, high):
             nonlocal result
             # base case
@@ -21,6 +22,21 @@ class Solution:
                 result+=root.val
             if root.right is not None and root.val<high:
                 helper(root.right, low, high)
+        
+        # integer based recursion
+        def helper2(root, low, high):
+            # base case
+            if root is None:
+                return 0
             
-        helper(root, low, high)
-        return result
+            # logic
+            temp=0
+            left = helper2(root.left, low, high)
+            if root.val >= low and root.val <= high:
+                temp = root.val
+            right = helper2(root.right, low, high)
+            return temp+left+right
+            
+        return helper2(root, low, high)
+        # helper(root, low, high)
+        # return result
